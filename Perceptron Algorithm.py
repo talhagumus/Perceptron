033bc619e98aaca993dcc73c64bd034d5ec3a013
talhagumus <115897230+talhagumus@users.noreply.github.com>
2023-08-18 00:05:55 +0300

    Add files via upload

---
+++ Perceptron Algorithm.py
@@ -0,0 +1,14 @@
+İnputs=[int(x) for x in input("Enter the input values").split()]
+weights=[int(w) for w in input("Enter the weight values").split()]
+treshold=int(input("Enter the treshold"))
+def step(weighted_sum):
+    if weighted_sum>treshold:
+        return 1
+    else:
+        return 0
+def perceptron():
+    weighted_sum=0
+    for x,w in zip(İnputs,weights)  :
+        weighted_sum+=x*w
+    return step(weighted_sum)  
+print(perceptron())            
